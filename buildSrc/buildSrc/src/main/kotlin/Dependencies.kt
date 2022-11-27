24199ada36a6b0c78c969997a30b31f899669b1b
Dmitry Seloustev <seloustevdv@elocont.ru>
2022-11-27 15:45:52 +0700

    Хочу попробовать в IDEA

--- buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,21 @@
 object Dependencies {
 
     object Kotlin {
-        private const val version = "1.7.10"
+        private const val version = "1.7.20"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+
+        object Serialization {
+            private const val plugin = "1.6.21"
+            const val gradlePlugin = "org.jetbrains.kotlin:kotlin-serialization:$plugin"
+
+            private const val version = "1.4.1"
+            const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:$version"
+        }
+
+        object Coroutines {
+            private const val version = "1.6.4"
+            const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        }
     }
 
     object Compose {
@@ -13,5 +26,23 @@ object Dependencies {
     object Android {
         private const val version = "7.3.1"
         const val gradlePlugin = "com.android.tools.build:gradle:$version"
+    }
+
+    object Kodein {
+        private const val version = "7.16.0"
+        const val core = "org.kodein.di:kodein-di:$version"
+    }
+
+    object Ktor {
+        private const val version = "2.1.3"
+        const val core = "io.ktor:ktor-client-core:$version"
+        const val json = "io.ktor:ktor-client-json:$version"
+        const val ios = "io.ktor:ktor-client-ios:$version"
+        const val negotiation = "io.ktor:ktor-client-content-negotiation:$version"
+        const val kotlin_json = "io.ktor:ktor-serialization-kotlinx-json:$version"
+        const val serialization = "io.ktor:ktor-client-serialization:$version"
+        const val logging = "io.ktor:ktor-client-logging:$version"
+        const val android = "io.ktor:ktor-client-android:$version"
+        const val okhttp = "io.ktor:ktor-client-okhttp:$version"
     }
 }
---
+++ common/core/src/commonMain/kotlin/ktor/KtorModule.kt
@@ -0,0 +1,35 @@
+package ktor
+
+import io.ktor.client.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.singleton
+
+internal val ktorModule = DI.Module("ktorModule") {
+    bind<HttpClient>() with singleton {
+        HttpClient(HttpEngineFactory().createEngine()) {
+            install(Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.ALL
+            }
+
+            install(ContentNegotiation) {
+                json(Json {
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                    prettyPrint = true
+                })
+            }
+
+            install(HttpTimeout) {
+                connectTimeoutMillis = 15_000
+                requestTimeoutMillis = 30_000
+            }
+        }
+    }
+}
